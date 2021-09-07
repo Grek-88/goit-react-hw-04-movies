5fa033b9fd3d4137c814790678318d062ff6e08a
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-09-07 21:15:40 +0300

    сделал fetch start

--- src/components/API/ApiFetch.jsx
+++ src/components/API/ApiFetch.jsx
@@ -1,4 +1,13 @@
-export default function ApiFetch() {
-  const result = fetch("HomePage");
+export default function ApiFetch(page = 1) {
+  const result = fetch(
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=b935b76f7270f1257638a990b1cb347c&page=${page}`
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(
+      new Error("По такому запросу фильмов не найдено. Введите другой запрос.")
+    );
+  });
   return result;
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,3 +1,61 @@
+import ApiFetch from "../API/ApiFetch";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import s from "../HomePage/HomePage.module.css";
+
 export default function HomePage() {
-  return <div>111</div>;
+  const [movieStart, setMovieStart] = useState([]);
+  // const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+
+  ApiFetch()
+    .then((moviesData) => {
+      console.log(moviesData.results);
+      setMovieStart((movieStart) =>
+        movieStart ? [...movieStart, ...moviesData.results] : moviesData.results
+      );
+    })
+    .catch((error) => setError(error));
+
+  console.log(error);
+
+  return (
+    <div>
+      <ul className={s.list}>
+        {movieStart.map((el) => {
+          return (
+            <li key={el.id} className={s.list_item}>
+              <Link
+                to={{
+                  pathname: `/${el.id}`,
+                  state: { from: "/home" },
+                }}
+              >
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${el.poster_path}`}
+                  alt={el.title}
+                />
+                <p>{el.title}</p>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
+
+// ((imgDataFetch) => {
+//   console.log(imgDataFetch);
+//   console.log(page);
+//   setPage(page + 1);
+//   setImgData((imgData) =>
+//     imgData ? [...imgData, ...imgDataFetch.hits] : imgDataFetch.hits
+//   );
+//   setLoading(false);
+// window.scrollTo({
+//   top: document.documentElement.scrollHeight,
+//   behavior: "smooth",
+// });
+// })
+//   .catch((error) => setError(error));
