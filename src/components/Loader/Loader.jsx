a2c714026c1480b7df2f336e90ac4584d20b40ef
Grek-88 <73738671+Grek-88@users.noreply.github.com>
2021-10-13 21:32:36 +0300

    update

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Loader from "react-loader-spinner";
+import s from "../Loader/Loader.module.css";
 
 export default class Spiner extends React.Component {
   render() {
     return (
-      <div style={{ marginLeft: "40%", marginTop: "20px" }}>
+      <div className={s.loader}>
         <Loader
           type="Bars"
           color="#408cff"
